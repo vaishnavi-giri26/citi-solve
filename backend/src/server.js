38e1cb898bb9cf2563810d68e68713e0d3b0a9e1
GIRI VAISHNAVI <vaishnavigiri2612@gmail.com>
2026-03-11 15:21:05 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,4 @@
 import express from "express";
-import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
 
@@ -17,28 +16,25 @@ connectdatabase();
 /* middleware */
 app.use(express.json());
 
-/* CORS FIX */
 app.use(cors({
-origin: "*",
+origin: [
+"http://localhost:5173",
+"https://citi-solve-frontend-gbqd.onrender.com"
+],
 methods: ["GET","POST","PUT","DELETE","OPTIONS"],
-allowedHeaders: ["Content-Type","Authorization"]
+allowedHeaders: ["Content-Type","Authorization"],
+credentials: true
 }));
 
-/* allow preflight */
 app.options("*", cors());
 
 /* routes */
 app.use("/api/auth", authRoutes);
 app.use("/api/complaints", complaintRoutes);
 
-/* server start */
+/* start server */
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-console.log("MongoDB Connected");
 app.listen(PORT, () => {
 console.log(`Server running on port ${PORT}`);
 });
-})
-.catch(err => console.log(err));
