9c58c572412ea6696b5d4621154325d0434d1d5d
GIRI VAISHNAVI <vaishnavigiri2612@gmail.com>
2026-03-11 14:46:38 +0530

    Add comment section for auth routes

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -3,6 +3,8 @@ import { register, login } from "../controllers/authController.js";
 
 const router = express.Router();
 
+/* ---------- AUTH ROUTES ---------- */
+
 router.post("/register", register);
 router.post("/login", login);
 
