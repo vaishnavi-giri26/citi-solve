61e3867bfeb46119321ac43e9a0118c865fcec7d
GIRI VAISHNAVI <vaishnavigiri2612@gmail.com>
2026-03-11 20:33:11 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "/api",
+  baseURL: "https://citi-solve-backend.onrender.com/api",
   headers: {
     "Content-Type": "application/json"
   }
