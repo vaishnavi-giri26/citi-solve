78dc0c57ff8336e7548bebc52d0bd5cded222669
GIRI VAISHNAVI <vaishnavigiri2612@gmail.com>
2026-03-11 21:04:35 +0530

    Update authController.js

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -17,7 +17,6 @@ process.env.JWT_SECRET,
 export const register = async (req, res) => {
 try {
 
-```
 const { name, email, password, role } = req.body;
 
 /* check existing user */
@@ -48,17 +47,17 @@ res.status(201).json({
   role: user.role,
   token: generateToken(user._id, user.role)
 });
-```
+
 
 } catch (error) {
 
-```
+
 console.error("REGISTER ERROR:", error);
 
 res.status(500).json({
   message: "Server error during registration"
 });
-```
+
 
 }
 };
@@ -70,7 +69,7 @@ export const login = async (req, res) => {
 
 try {
 
-```
+
 const { email, password } = req.body;
 
 /* find user */
@@ -98,17 +97,16 @@ res.status(200).json({
   role: user.role,
   token: generateToken(user._id, user.role)
 });
-```
+
 
 } catch (error) {
 
-```
 console.error("LOGIN ERROR:", error);
 
 res.status(500).json({
   message: "Server error during login"
 });
-```
+
 
 }
 
