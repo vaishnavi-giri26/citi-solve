14ac8b95c8e23821285dc89dfd99b15553071a48
GIRI VAISHNAVI <vaishnavigiri2612@gmail.com>
2026-03-11 20:11:16 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,63 +1,60 @@
 import axios from "axios";
 
 const API = axios.create({
-baseURL: "https://citi-solve-backend.onrender.com/api",
-headers: {
-"Content-Type": "application/json"
-}
+  baseURL: "/api",
+  headers: {
+    "Content-Type": "application/json"
+  }
 });
 
 API.interceptors.request.use((req) => {
-const userData = localStorage.getItem("user");
+  const userData = localStorage.getItem("user");
 
-if (userData) {
-const user = JSON.parse(userData);
+  if (userData) {
+    const user = JSON.parse(userData);
 
-```
-if (user && user.token) {
-  req.headers.Authorization = "Bearer " + user.token;
-}
-```
+    if (user && user.token) {
+      req.headers.Authorization = "Bearer " + user.token;
+    }
+  }
 
-}
-
-return req;
+  return req;
 });
 
 /* AUTH APIs */
 
 export const authAPI = {
-register: async (data) => {
-const res = await API.post("/auth/register", data);
-return res.data;
-},
+  register: async (data) => {
+    const res = await API.post("/auth/register", data);
+    return res.data;
+  },
 
-login: async (data) => {
-const res = await API.post("/auth/login", data);
-return res.data;
-}
+  login: async (data) => {
+    const res = await API.post("/auth/login", data);
+    return res.data;
+  }
 };
 
 /* COMPLAINT APIs */
 
 export const complaintsAPI = {
-createComplaint: async (data) => {
-const res = await API.post("/complaints", data);
-return res.data;
-},
+  createComplaint: async (data) => {
+    const res = await API.post("/complaints", data);
+    return res.data;
+  },
 
-getMyComplaints: async () => {
-const res = await API.get("/complaints/my");
-return res.data;
-},
+  getMyComplaints: async () => {
+    const res = await API.get("/complaints/my");
+    return res.data;
+  },
 
-getComplaints: async () => {
-const res = await API.get("/complaints");
-return res.data;
-},
+  getComplaints: async () => {
+    const res = await API.get("/complaints");
+    return res.data;
+  },
 
-updateStatus: async (id, status) => {
-const res = await API.put(`/complaints/${id}/status`, { status });
-return res.data;
-}
+  updateStatus: async (id, status) => {
+    const res = await API.put(`/complaints/${id}/status`, { status });
+    return res.data;
+  }
 };
