2564eac29d2ca610d14b35f6dda02f8ac6675dbf
GIRI VAISHNAVI <vaishnavigiri2612@gmail.com>
2026-03-16 22:28:22 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,13 +11,13 @@ dotenv.config();
 const app = express();
 
 /* CORS MUST COME FIRST */
-app.use(cors({
-origin: "https://citi-solve-frontend-gbqd.onrender.com",
-credentials: true
-}));
-
-/* Handle preflight */
-app.options("*", cors());
+app.use(
+  cors({
+    origin: "https://citi-solve-frontend-gbqd.onrender.com",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 /* Middleware */
 app.use(express.json());
@@ -31,12 +31,12 @@ app.use("/api/complaints", complaintRoutes);
 
 /* Test route */
 app.get("/", (req, res) => {
-res.send("API is running...");
+  res.send("API is running...");
 });
 
 /* Start server */
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
